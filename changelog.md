dd44500b80a74b73cf93b8d26bc0a0ebb861fbec
Welsey0 <welsey0@github.com>
2025-11-11 17:42:47 -0800

    Major update: 3.1.0

--- changelog.md
+++ changelog.md
@@ -1,20 +1,3 @@
-### Updated Mods
-- Chat Heads: [1\.1\.5]
-- Euphoria Patches: [1\.7\.6-r5\.6\.1]
-- Smooth Scrolling: [2\.4\.0]
-- Entity Model Features: [3\.0\.5]
-- LambDynamicLights - Dynamic Lights: [4\.8\.6]
-- Resourcify: [1\.7\.5]
-- Shulker Box Tooltip: [5\.2\.12]
-- Fabric API: [0\.138\.3]
-- Sodium: [0\.7\.3]
-- BadOptimizations: [2\.3\.4]
-- Entity Culling: [1\.9\.3]
-- FerriteCore: [8\.0\.2]
-- Crash Assistant: [1\.10\.19]
-
-### Updated Packs
-- Rethoughted GUI
-
-### Notes
-Future changelogs will no longer list specific mods or updated versions as it has become a major chore for each release.
+- ViaFabricPlus updated
+- Continuity readded
+- Debugify readded (Actions build)
--- modlist.sh
+++ modlist.sh
@@ -10,10 +10,12 @@ modlist=(
     chat-impressive-animation
     cloth-config
     clumps
+    continuity
     controlling
     crash-assistant
     cubes-without-borders
     dark-loading-screen
+    #debugify # Actions build
     dynamic-fps
     entity-model-features
     entitytexturefeatures
@@ -22,19 +24,20 @@ modlist=(
     fabric-api
     fabric-language-kotlin
     fast-ip-ping
-    fastquit
+    #fastquit # Actions build
     ferrite-core
     forge-config-api-port
     immediatelyfast
     iris
-    krypton # Temporarily removed for a crash in the newest version 10/27/2025
+    #krypton # Temporarily removed for a crash in the newest version 10/27/2025
     lambdynamiclights
+    #language-reload # Currently incompatible
     lithium
     main-menu-credits
     mixintrace
     modmenu
     modelfix
-    mutils # Have been using my own (still not accepted) PR since 1.21.8
+    #mutils # Have been using my own (still not accepted) PR since 1.21.8
     morechathistory
     moreculling
     no-chat-reports
@@ -55,7 +58,7 @@ modlist=(
     sodium-extra
     spark
     stackdeobf
-    threadtweak
+    #threadtweak # Actions build
     tooltip-scroll
     viafabricplus
     yacl
